25b3bfc9d9c964e61bac6f8950c173b16cf770ad
Raphael Hilario <rapha_hilario.89@hotmail.com>
2023-12-26 23:59:34 -0300

    objetos_aula1

---
+++ Objetos/Aula1/alterar.js
@@ -0,0 +1,31 @@
+const pessoa2 = {
+    nome: "Luma",
+    profissao: "Engenheira",
+}
+
+//console.log(pessoa.nome);
+
+//console.log(pessoa.telefone);
+
+//pessoa.telefone = "11 996196073";
+
+//console.log(pessoa.telefone);
+
+//pessoa.nome = "Luma Silva";
+
+//console.log(pessoa);
+
+const pessoa = {
+    nome: "Bruce Banner",
+    dataNascimento: "25/01/1980",
+    carteiraIdentidade: "997776-X",
+    email: "profbanner@email.com",
+    telefone: "+552877776666",
+    cidade: "Cachoeiro de Itapemirim",
+    estado: "ES"
+}
+
+pessoa.cpf = "12345678".substring(0, 4);
+pessoa.seguroSocial = "98654321";
+
+console.log(pessoa);
---
+++ Objetos/Aula1/objeto.js
@@ -0,0 +1,10 @@
+const listaCPFs = [1111111, 22222222, 33333333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF", "1111111111"];
+
+console.log(informacoesPessoa);
+
+const objetoPessoa = {
+    nome: "Jose",
+    idade: 32
+};
